32e000a3c81011ef6f3c161cfe027459b549ba92
Scriptkiddi <fritz@otlinghaus.it>
2021-01-28 17:28:37 +0100

    Rework extension

--- content_script.js
+++ content_script.js
@@ -1,10 +1,16 @@
-var regexPatternPhone = /(([\+][0-9]{1,3})|([0-9])+)?[\s]?(([0-9])+[\s]?){6,20}/g;
+chrome.storage.local.get({
+        helsinkiCallRegEx: "(([\+][0-9]{1,3})|([0-9])+)?[\s]?(([0-9])+[\s]?){6,20}",
+        helsinkiCallProtocol: "tel:",
+}, function (items) {
+    settings = items;
+	$('p').each(function( index ) {
+		text = $(this).html();
+		$(this).html(text.replace(new RegExp(items.helsinkiCallRegEx, 'g'), function(x){
+			return "<a href='"+items.helsinkiCallProtocol+x.replace(/ /g, '')+"'>"+ x +"</a>";
+		}))
+	});
 
-$('p').each(function( index ) {
-	text = $(this).html();
-	$(this).html(text.replace(regexPatternPhone, function(x){
-		return "<a href='tel:"+x+"'>"+ x +"</a>";
-	}))
 });
+
 
 
--- options.js
+++ options.js
@@ -6,13 +6,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 var settings = null;
 
 function save_options() {
-    var linkFormatValue = document.getElementById("telLinkFormat").value;
-    var linkTextFormatValue = document.getElementById("linkTextFormat").value;
-    var overrideValue = document.getElementById("overrideLinks").checked;
+    var linkFormatValue = document.getElementById("helsinkiCallRegEx").value;
+    var linkTextFormatValue = document.getElementById("helsinkiCallProtocol").value;
     chrome.storage.local.set({
-        telLinkFormat: linkFormatValue,
-        linkTextFormat: linkTextFormatValue,
-        overrideLinks: overrideValue
+        helsinkiCallRegEx: linkFormatValue,
+        helsinkiCallProtocol: linkTextFormatValue,
     }, function () {
         // Update status to let user know options were saved.
         var status = document.getElementById("status");
@@ -26,86 +24,13 @@ function save_options() {
 
 function restore_options() {
     chrome.storage.local.get({
-        telLinkFormat: "tel:+1-{1}-{2}-{3}",
-        linkTextFormat: "{0}",
-        overrideLinks: true,
-        ignoredDomains: [],
-        ignoredURLS: []
+        helsinkiCallRegEx: "(([\+][0-9]{1,3})|([0-9])+)?[\s]?(([0-9])+[\s]?){6,20}",
+        helsinkiCallProtocol: "tel:",
     }, function (items) {
         settings = items;
-        document.getElementById("telLinkFormat").value = items.telLinkFormat;
-        document.getElementById("linkTextFormat").value = items.linkTextFormat;
-        document.getElementById("overrideLinks").checked = items.overrideLinks;
-
-        restoreDomains();
-        restoreURLS();
+        document.getElementById("helsinkiCallRegEx").value = items.helsinkiCallRegEx;
+        document.getElementById("helsinkiCallProtocol").value = items.helsinkiCallProtocol;
     });
 }
 
-function restoreDomains()
-{
-    if (settings.ignoredDomains.length == 0) {
-        document.getElementById("filteredDomainsTR").remove();
-    }
-    else {
-        var domainList = document.getElementById("filteredDomainsList");
-        for (var i = 0; i < settings.ignoredDomains.length; i++) {
-            var item = document.createElement("li");
-            var hideButton = document.createElement("div");
-            hideButton.className = "hide-button";
-            hideButton.appendChild(document.createTextNode("X"));
-            item.title = settings.ignoredDomains[i];
-            item.appendChild(hideButton);
-            item.appendChild(document.createTextNode(decodeURI(settings.ignoredDomains[i])));
-            domainList.appendChild(item);
-            item.onclick = handleDomainRemoval;
-        }
-    }
-}
 
-function restoreURLS()
-{
-    if (settings.ignoredURLS.length == 0) {
-        document.getElementById("filteredURLSTR").remove();
-    }
-    else {
-        var urlList = document.getElementById("filteredURLList");
-        for (var i = 0; i < settings.ignoredURLS.length; i++) {
-            var item = document.createElement("li");
-            var hideButton = document.createElement("div");
-            hideButton.className = "hide-button";
-            hideButton.appendChild(document.createTextNode("X"));
-            item.title = settings.ignoredURLS[i];
-            item.appendChild(hideButton);
-            item.appendChild(document.createTextNode(decodeURI(settings.ignoredURLS[i])));
-            urlList.appendChild(item);
-            item.onclick = handleURLRemoval;
-        }
-    }
-}
-
-function handleDomainRemoval(event)
-{
-    var data = event.target.textContent.substring(1);
-    settings.ignoredDomains.splice(settings.ignoredDomains.indexOf(data), 1);
-    chrome.storage.local.set({
-        ignoredDomains: settings.ignoredDomains
-    }, function () {
-        event.target.remove();
-        if (settings.ignoredDomains.length == 0)
-            document.getElementById("filteredDomainsTR").remove();
-    });
-}
-
-function handleURLRemoval(event)
-{
-    var data = event.target.textContent.substring(1);
-    settings.ignoredURLS.splice(settings.ignoredURLS.indexOf(data), 1);
-    chrome.storage.local.set({
-        ignoredURLS: settings.ignoredURLS
-    }, function () {
-        event.target.remove();
-        if (settings.ignoredURLS.length == 0)
-            document.getElementById("filteredURLSTR").remove();
-    });
-}
